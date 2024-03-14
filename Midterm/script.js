1b2b844621ea92d2e42d15e5a3c42d44d8d3d112
Vivian Teo <vivianteo950@gmail.com>
2024-03-14 01:16:24 -0400

    finishing touches to media queries

--- Midterm/script.js
+++ Midterm/script.js
@@ -1,7 +1,12 @@
 var mobileIcon = document.getElementsByClassName('mobile-menu');
 var navBar = document.getElementsByClassName('menu');
+var contactIcons = document.getElementsByClassName('contact-icon');
 
 mobileIcon[0].addEventListener('click', toggleMobile);    
+
+// for (i=0; i<contactIcons.length; i++) {
+//     contactIcons[i].addEventListener('mouseover', hoverContactIcons(i));
+// }
 
 function toggleMobile() {
     console.log('Toggling');
@@ -13,3 +18,8 @@ function toggleMobile() {
         navBar[0].style.display = 'none';
     }
 }
+
+// function hoverContactIcons(i) {
+//     console.log("hovering");
+//     contactIcons[i].src="./img/"+contactIcons[i].id+"_hover"+".png";
+// }
